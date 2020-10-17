868ac3ee803867d2f9f9da11fe6f633f3cb0a2f9
“alka-si” <alkasimon23@gmail.com>
2020-10-18 04:29:42 +0530

    Added states and functionalities for all the actions

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -7,16 +8,47 @@ import './Blog.css';
 import background from '../../assets/images/background.jpg'
 
 class Blog extends Component {
+    state = {
+        blogs: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    getPostBodyHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    componentDidMount(){
+        axios.get("/posts")
+            .then(response => {
+                const posts = response.data.slice(0,4)
+                const updatedPosts = posts.map((post) => {
+                    return{
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({blogs: updatedPosts})
+            })
+            .catch(() => {
+                this.setState({error: true})
+            })
+    }
+
     render () {
+        let posts = this.state.blogs.map((blog) => {
+            return <Post key={blog.id} author={blog.author} title={blog.title} getPost={() => this.getPostBodyHandler(blog.id)}/>
+        })
+        if(this.state.error){
+            posts = <p style={{textAlign: 'center'}}>Something went wrong...!</p>
+        }
         return (
             <div className='Blog' style={{backgroundImage: `url(${background})`}}>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
